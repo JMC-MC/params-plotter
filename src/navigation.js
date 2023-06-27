e5fabe1fc18e8366a9ce9cd70cbfda366f8880b4
Joe <joecurrin@gmail.com>
2023-06-27 10:02:11 +1000

    RWA-53 fixed by changing from display to visibility CSS attribute

--- src/navigation.js
+++ src/navigation.js
@@ -1,49 +1,35 @@
 // When document is ready
-
 $(function () {
   // Radar screen is active on load
   $('#radar-button').addClass('activeclicked'); // Make radar-button active on page load
+  // Switch Screens
+  $('#radar').css('visibility', 'visible');
+  $('#lookOut').css('visibility', 'hidden');
+  $('#rules-view').css('visibility', 'hidden');
 
   // Navigation
   $('#lookout-button').on('touchstart mousedown', function () {
-    // clear();
     setActiveMenu(this);
     // Switch Screens
-    $('#radar').hide();
-    $('#lookOut').show();
-    $('#rules-view').hide();
-    $('#staff-answer').hide();
+    $('#radar').css('visibility', 'hidden');
+    $('#lookOut').css('visibility', 'visible');
+    $('#rules-view').css('visibility', 'hidden');
   });
 
   $('#radar-button').on('touchstart mousedown', function () {
     setActiveMenu(this);
     // Switch Screens
-    $('#radar').show();
-    $('#lookOut').hide();
-    $('#rules-view').hide();
-    $('#report-view').hide();
-    $('#staff-answer').hide();
+    $('#radar').css('visibility', 'visible');
+    $('#lookOut').css('visibility', 'hidden');
+    $('#rules-view').css('visibility', 'hidden');
   });
 
   $('#rules-button').on('touchstart mousedown', function () {
     setActiveMenu(this);
     // Switch Screens
-    $('#radar').hide();
-    $('#lookOut').hide();
-    $('#rules-view').show();
-    $('#staff-answer').hide();
-  });
-
-  $('#edit-button').on('touchstart mousedown', function () {
-    setActiveMenu(this);
-    // Switch Screens
-    $('#radar').show();
-    $('#lookOut').hide();
-    $('#rules-view').hide();
-    $('#analysis-view').hide();
-    $('#ctrl-bar').show();
-    $('#debrief-view').hide();
-    updateTgtList();
+    $('#radar').css('visibility', 'hidden');
+    $('#lookOut').css('visibility', 'hidden');
+    $('#rules-view').css('visibility', 'visible');
   });
 
   // Report View
